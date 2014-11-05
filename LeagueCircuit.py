73ebf21eddd4d54c6e04741f2031890b733077ea
Rob <Rob.eric.cole@gmail.com>
2014-11-05 16:01:37 -0500

    Created pages for everything. Connected pages to eachother through links.

--- LeagueCircuit.py
+++ LeagueCircuit.py
@@ -11,6 +11,21 @@ def index():
 def sign_up():
     return render_template("signup.html")
 
+@app.route('/home')
+def home():
+    return render_template("home.html")
+
+@app.route('/log-in')
+def log_in():
+    return  render_template("login.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 if __name__ == '__main__':
     app.run(debug=True)

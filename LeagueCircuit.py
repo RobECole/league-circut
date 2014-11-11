2efde7bc76996209275c22892d605e6844dc01e8
Rob <Rob.eric.cole@gmail.com>
2014-11-10 20:24:03 -0500

    Modified the CSS to correct issues with the footer. Created forms using WTForms. Created a re-direct from the sign in and sign up page. Starting flash() for error and success messages

--- LeagueCircuit.py
+++ LeagueCircuit.py
@@ -1,23 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash
+from flask.ext.wtf import Form
+from wtforms import StringField, PasswordField
 
 app = Flask(__name__)
-
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return render_template("signup.html")
+    class SignUpForm(Form):
+        email = StringField()
+        password = PasswordField()
+        confirm = PasswordField()
+        summoner_name = StringField()
+
+    form = SignUpForm()
+    if form.validate_on_submit():
+        flash("success")
+        # Call some function to check if new
+        # IF NOT UNIQUE FLASH ERROR
+        # else get summoners ID
+        return redirect('home')
+
+
+    return render_template("signup.html", form=form)
+
 
 @app.route('/home')
 def home():
     return render_template("home.html")
 
-@app.route('/log-in')
+@app.route('/log-in', methods=['GET', 'POST'])
 def log_in():
-    return  render_template("login.html")
+    class LogInForm(Form):
+        email = StringField()
+        password = PasswordField()
+
+    form = LogInForm()
+    if form.validate_on_submit():
+        print form.email.data, form.password.data
+
+    return render_template("login.html", form=form)
 
 @app.route('/about')
 def about():

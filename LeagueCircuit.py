bb5dfc0d8c583dacb5e99efc65bff9a2b60157d0
Orobas <peters_a@live.com>
2014-11-23 11:58:17 -0500

    Changed the login where it can query the password and username from the database and match it with the information typed in the stringfield and passwordfield.  Added sign-up functionally, account information is added to the database now.

--- LeagueCircuit.py
+++ LeagueCircuit.py
@@ -1,7 +1,6 @@
 import psycopg2
-import sys
-import pprint
-from flask import Flask, render_template, redirect, flash
+import requests
+from flask import Flask, render_template, redirect, flash, session, escape, request
 from flask.ext.wtf import Form
 from wtforms import StringField, PasswordField
 
@@ -12,6 +11,7 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 def index():
     return render_template("index.html")
 
+
 @app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     class SignUpForm(Form):
@@ -19,45 +19,43 @@ def sign_up():
         password = PasswordField()
         confirm = PasswordField()
         summoner_name = StringField()
-
     form = SignUpForm()
     if form.validate_on_submit():
-        mail = form.email.data.encode('ascii','ignore')
+        mail = form.email.data.encode('ascii', 'ignore')
         mail = mail.lower()
-        name = form.summoner_name.data.encode('ascii','ignore')
+        name = form.summoner_name.data.encode('ascii', 'ignore')
         name = name.lower()
         connect.cursor.execute("SELECT username FROM LEAGUE.USER WHERE username = '{0}'".format(mail))
-        uniqueuser = connect.cursor.fetchall()
-        connect.cursor.execute("SELECT sum_name FROM LEAGUE.USER WHERE sum_name = '{0}'".format(name))
-        uniquename = connect.cursor.fetchall()
-        if uniqueuser:
-            print "user id taken"
-            return redirect('sign-up')
+        err = 0
+        if connect.cursor.fetchall():
+            flash('User id already taken.')
+            err = 1
+            #return redirect('sign-up')
             #need to put error message
+        connect.cursor.execute("SELECT sum_name FROM LEAGUE.USER WHERE sum_name = '{0}'".format(name))
+        if connect.cursor.fetchall():
+            flash('Summoner name already registered.')
+            err = 1
+            #return redirect('sign-up')
+        if form.password.data.encode('ascii', 'ignore') != form.confirm.data.encode('ascii', 'ignore'):
+            flash('Passwords do not match.')
+            err = 1
+        if err == 0:
+            connect.cursor.execute("INSERT INTO LEAGUE.USER VALUES ('{0}','{1}','{2}')".format(mail, form.password.data.encode('ascii', 'ignore'), name))
+            connect.conn.commit()
+            redirect('log-in')
+            return flash('Sign up successful!')
         else:
-            if uniquename:
-                print "summoner name taken"
-                return redirect('sign-up')
-            else:
-                if form.password.data.encode('ascii','ignore') == form.confirm.data.encode('ascii','ignore'):
-                    connect.cursor.execute("INSERT INTO LEAGUE.USER VALUES ('{0}','{1}','{2}')".format(mail,form.password.data.encode('ascii','ignore'),name))
-                    connect.conn.commit()
-                    names = connect.cursor.execute("SELECT * FROM LEAGUE.USER WHERE username = '{0}'".format(mail))
-                    print names
-                    return redirect('home')
-                else:
-                    print "passwords don't match"
-                    return redirect('sign-up')
-
-
-
-
+            return redirect('sign-up')
     return render_template("signup.html", form=form)
 
 
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    if 'username' in session:
+        return render_template("home.html")
+    return redirect("log-in"), flash('You are not logged in')
+
 
 @app.route('/log-in', methods=['GET', 'POST'])
 def log_in():
@@ -66,26 +64,25 @@ def log_in():
         password = PasswordField()
 
         def __str__(self):
-            return  form.email.data
+            return form.email.data
 
     form = LogInForm()
     if form.validate_on_submit():
-        #print form.email.
-        #qrrystr = "SELECT * FROM LEAGUE.USER WHERE username = (%s)", (form.email)# + form.email#connect.cursor.execute("SELECT * FROM LEAGUE.USER WHERE username = email()")
-        value = form.email.data.encode('ascii','ignore')
+        value = form.email.data.encode('ascii', 'ignore')
         value = value.lower()
-        print value
         connect.cursor.execute("SELECT * FROM LEAGUE.USER WHERE username = '{0}'" .format(value))
         records = connect.cursor.fetchall()
-        password = form.password.data.encode('ascii','ignore')
-        if records[0][1] == password:
-            sumname = records[0][2]
-            #valid session = true
-            return redirect('home')
-
-    else:
-        #flash error
-        return render_template("login.html", form=form)
+        password = form.password.data.encode('ascii', 'ignore')
+        if records:
+            if records[0][1] == password:
+                sumname = records[0][2]
+                #valid session = true
+                session['username'] = value
+                print session['username']
+                return redirect('home')
+        else:
+            #flash error
+            return render_template("login.html", form=form)
     return render_template("login.html", form=form)
 
 @app.route('/about')
@@ -98,12 +95,8 @@ def contact():
 
 class connect():
     conn_string = "host='localhost' dbname='LEAGUE_CIRCUIT' user='postgres' password='RAMP'"
-    print "connecting to database\n ->%s" % (conn_string)
     conn = psycopg2.connect(conn_string)
     cursor = conn.cursor()
-    #cursor.execute("SELECT Summoner_id FROM LEAGUE.PLAYER WHERE Summoner_id = 48445998")
-    #records = cursor.fetchall()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
